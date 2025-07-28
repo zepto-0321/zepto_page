17af44e5de915de17a0bc658a5ee4cd20e43505f
zepto-0321 <yiyoushiye@gmail.com>
2025-07-28 21:43:53 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+var wasteWords = ["varを使って何が悪い!","石と意思が充実した毎日が幸せのカギ","これであと10年は戦える！ー匿名Sさん","marqeeタグを使いまくれ！！","男ならメモ帳一択だ！","男ならアセンブリだ！","SETP4 プルトニウムから出る放射線をベリリウムで反射させます"];
+onload = function(){
+  var ww = document.getElementById("wasteWords");
+  ww.textContent = wastewords[getRandomInt(0,wasteWords.length)];
+}
+
+
+function getRandomInt(min, max) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // 上限は除き、下限は含む
+}
